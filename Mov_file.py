ea1260eb61ec3129fbb2e1e2240a1d198d900590
Komal123029 <105293426+Komal123029@users.noreply.github.com>
2023-07-13 19:51:09 +0530

    Add files via upload

--- Mov_file.py
+++ Mov_file.py
@@ -9,5 +9,24 @@ list_of_files = os.listdir(from_dir)
 
 for i in list_of_files:
    root,ext = os.path.splitext(i)
-   print(root)
-   print(ext)
+   #print(root)
+   #print(ext)
+
+   if(ext == " "):
+    continue
+   if(ext in [".txt",".doc",".docx",".pdf"]):
+    path1 = from_dir + "/" + i
+    path2 = to_dir + "/" + "document_files"
+    path3 = path2 +  "/" + i  
+    if os.path.exists(path2):
+      print("moving " + i)
+      shutil.move(path1,path3)
+    else:
+      os.makedirs(path2)
+      print("moving " + i)
+      shutil.move(path1,path3)
+        
+
+   
+
+      
